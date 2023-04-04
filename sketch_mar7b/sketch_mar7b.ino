f7948a3db5027d4e7fed49e321b4fa88fa38af0d
runzac <91915526+runzac@users.noreply.github.com>
2023-04-04 21:37:49 +0200

    Add 2 Keypad (4x4) & double check of input

--- sketch_mar7b/sketch_mar7b.ino
+++ sketch_mar7b/sketch_mar7b.ino
@@ -4,32 +4,44 @@
 const byte ROWS = 4; //righe
 const byte COLS = 4; //colonne
 
-char tasti[ROWS][COLS] = {
-  {'A', 'B', 'C', 'D'},
-  {'E', 'F', 'G', 'U'},
-  {'I', 'L', 'M', 'N'},
-  {'O', 'P', 'Q', 'R'}
+char keys[ROWS][COLS] = {
+  {'A','B','C','D'},
+  {'I','J','K','L'},
+  {'Q','R','S','T'},
+  {'Y','Z','K','L'},
 };
 
-byte rowPins[ROWS] = {13, 12, 11, 10};
-byte colPins[COLS] = {9, 8, 7, 6};
+char keys2[ROWS][COLS] = {
+  {'E','F','G','H'},
+  {'M','N','O','P'},
+  {'U','V','W','X'},
+  {'B','N','M','D'},
+};
 
-Keypad customKeypad = Keypad(makeKeymap(tasti), rowPins, colPins, ROWS, COLS);
+byte rowPins[ROWS] = {29, 28, 27, 26};
+byte colPins[COLS] = {25, 24, 23, 22};
+byte rowPins2[ROWS] = {37, 36, 35, 34};
+byte colPins2[COLS] = {33, 32, 31, 30};
+
+Keypad firstKpd = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+Keypad secondKpd = Keypad(makeKeymap(keys2), rowPins2, colPins2, ROWS, COLS);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-//dichiarazioni
+//BISOGNA FARE I MENU (MENU INIZIALE, DI GIOCO E MENU DELA SCELTA DELLA DIFFICOLTA')
+//BISOGNA FARE I CONTROLLI PER OGNI LETTERA. (FATTO, MA DA FINIRE)
+//BISOGNA FARE LA STRINGA[VETTORE] CHE SERVE PER BANNARE LE PAROLE GIA USCITE DALLA STRINGA[VETTORE] CON TUTTE LE PAROLE.
+//METTERE UN BUZZ SUONO BELLO SE HA INDOVINATO LA LETTERA E BRUTTO SE LA SBAGLIA
 
-char inserimento;
-char pippo = ' ';
+//dichiarazioni
+char input, input2;
 String parola = "";
 String b [] = {
   #include "/Users/alerunza/Documents/Arduino/sketch_mar7b/parole.txt"
 };
 //String b [] = {"PAOLO", "MAMMA", "CAINGERO", "AACC"}; // parole da generare
-String a = ""; // parola da indovinare
-String nascosta = "";
-int i = 0, tentativi = 0, tent = 6, generated = 0;
-
+String a = ""; //parola da indovinare
+String hidden = "";
+int i = 0, tent = 6, generated = 0;
 bool flag = false;
 
 void setup(){
@@ -38,31 +50,32 @@ void setup(){
   lcd.init();
   lcd.backlight();
   generated = generaNum(); //variabile a cui viene assegnato il valore in uscita della funzione che genera il numero
-  
+
   a = b[generated];
 
   Serial.println(a + " | " + generated);
 
-  for(int i = 0; i < a.length(); i++){ //da testare se va o meno, su cpp va
-    nascosta += "*";
+  for(int i = 0; i < a.length(); i++){
+    hidden += "*";
   }
 
 }
 
 void loop(){
-  inserimento = customKeypad.getKey();
+  input = firstKpd.getKey();
+  input2 = secondKpd.getKey();
 
   lcd.setCursor(0, 0);
-  lcd.print("HangMan - ");
-  lcd.setCursor(10, 0);
-  lcd.print(nascosta);
+/*   lcd.print("HangMan - ");
+  lcd.setCursor(10, 0); */
+  lcd.print(hidden);
 
 /*   for(int x = 10; x < a.length()+10; x++){
     lcd.setCursor(x, 0);
     lcd.print("*");
   } */
 
-  if(inserimento){
+  if(input){
     if(flag){
       i = 0;
     }
@@ -70,23 +83,13 @@ void loop(){
       flag = false;
     }
     lcd.setCursor(i, 1);
-    lcd.print(inserimento);
-    parola += inserimento;
-    
-/*     for(int y = 0; y < a.length(); y++){
-      if(parola[i] == a[y]){
-        lcd.clear();
-        lcd.setCursor(2, 1);
-        lcd.print("giusto! " + parola[i]);
-        delay(1500);
-      }
-    } */
+    lcd.print(input);
+    parola += input;
 
     //controllo di ogni lettera inserita
     for(int s = 0; s < a.length(); s++){
-      if(inserimento == a[s]){
-        nascosta[s] = inserimento;
-        //Serial.println(nascosta + "" + tent);
+      if(input == a[s]){
+        hidden[s] = input;
         delay(500);
         tent++;
       } else{
@@ -96,38 +99,32 @@ void loop(){
     tent++;
     i++;
     delay(500);
-    if(nascosta == a){
+    if(hidden == a){
       i = a.length();
     }
     if(tent == i){
-      if(nascosta == a){
+      if(hidden == a){
         lcd.setCursor(0, 0);
         lcd.print("HangMan");
         lcd.setCursor(0, 1);
-        lcd.print(a + " | " + nascosta + " - WIN");
+        lcd.print(a + " | " + hidden + " - WIN");
         delay(2000);
         lcd.clear();
       } else{
         lcd.setCursor(0, 0);
         lcd.print("HangMan");
         lcd.setCursor(0, 1);
-        lcd.print(a + " | " + nascosta + " - LOST");
+        lcd.print(a + " | " + hidden + " - LOST");
         delay(2000);
         lcd.clear();
       }
     }
     if(i == a.length()){
       flag = true;
-/*       if(tent < 0){
-        lcd.clear();
-        lcd.setCursor(0, 1);
-        lcd.print("Sei MORTO");
-        delay(2000);
-      } */
 
       //check se la parola composta anche non in ordine è corretta
       lcd.clear();
-      if(nascosta == a){
+      if(hidden == a){
         lcd.setCursor(0, 0);
         lcd.print("HangMan");
         lcd.setCursor(0, 1);
@@ -142,8 +139,94 @@ void loop(){
         delay(2000);
         lcd.clear();
       }
+    }
 
-/*       if(parola == a){
+  }
+  if(input2){
+    if(flag){
+      i = 0;
+    }
+    if(i == 0){
+      flag = false;
+    }
+    lcd.setCursor(i, 1);
+    lcd.print(input2);
+    parola += input2;
+
+    //controllo di ogni lettera inserita
+    for(int s = 0; s < a.length(); s++){
+      if(input2 == a[s]){
+        hidden[s] = input2;
+        delay(500);
+        tent++;
+      } else{
+        //tent -= 1; // da fixare
+      }
+    }
+    tent++;
+    i++;
+    delay(500);
+    if(hidden == a){
+      i = a.length();
+    }
+    if(tent == i){
+      if(hidden == a){
+        lcd.setCursor(0, 0);
+        lcd.print("HangMan");
+        lcd.setCursor(0, 1);
+        lcd.print(a + " | " + hidden + " - WIN");
+        delay(2000);
+        lcd.clear();
+      } else{
+        lcd.setCursor(0, 0);
+        lcd.print("HangMan");
+        lcd.setCursor(0, 1);
+        lcd.print(a + " | " + hidden + " - LOST");
+        delay(2000);
+        lcd.clear();
+      }
+    }
+    if(i == a.length()){
+      flag = true;
+      //check se la parola composta anche non in ordine è corretta
+      lcd.clear();
+      if(hidden == a){
+        lcd.setCursor(0, 0);
+        lcd.print("HangMan");
+        lcd.setCursor(0, 1);
+        lcd.print(a + " - WIN");
+        delay(2000);
+        lcd.clear();
+      } else{
+        lcd.setCursor(0, 0);
+        lcd.print("HangMan");
+        lcd.setCursor(0, 1);
+        lcd.print(a + " - LOST");
+        delay(2000);
+        lcd.clear();
+      }
+    }
+
+  }
+}
+
+/*     for(int y = 0; y < a.length(); y++){
+      if(parola[i] == a[y]){
+        lcd.clear();
+        lcd.setCursor(2, 1);
+        lcd.print("giusto! " + parola[i]);
+        delay(1500);
+      }
+    } */
+
+    /*       if(tent < 0){
+        lcd.clear();
+        lcd.setCursor(0, 1);
+        lcd.print("Sei MORTO");
+        delay(2000);
+      } */
+
+    /*       if(parola == a){
         lcd.setCursor(3, 1);
         lcd.print(" - WIN");
         delay(2000);
@@ -155,15 +238,6 @@ void loop(){
         delay(2000);
         lcd.clear();
       } */
-      
-      //BISOGNA FARE I MENU (MENU INIZIALE, DI GIOCO E MENU DELA SCELTA DELLA DIFFICOLTA')
-      //BISOGNA FARE I CONTROLLI PER OGNI LETTERA. (FATTO, MA DA FINIRE)
-      //BISOGNA FARE LA STRINGA[VETTORE] CHE SERVE PER BANNARE LE PAROLE GIA USCITE DALLA STRINGA[VETTORE] CON TUTTE LE PAROLE.
-      //METTERE UN BUZZ SUONO BELLO SE HA INDOVINATO LA LETTERA E BRUTTO SE LA SBAGLIA
-    }
-
-  }
-}
 
 int generaNum(){      //funzione che genera un numero a caso seguendo un range 
   return random(0, 199);
