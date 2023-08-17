3c8665e881c216f620af8186e1995ea345a58ccc
runzac <91915526+alerunza@users.noreply.github.com>
2023-08-17 17:58:28 +0200

    Update sketch_aug12a.ino

--- sketch_aug12a/sketch_aug12a.ino
+++ sketch_aug12a/sketch_aug12a.ino
@@ -5,8 +5,11 @@
 
 #include <addons/RTDBHelper.h>
 
-#define WIFI_SSID "Wind3 2,4"
-#define WIFI_PASSWORD "siiwiyjv2kkjx2d"
+/* #define WIFI_SSID "Wind3 2,4"
+#define WIFI_PASSWORD "siiwiyjv2kkjx2d" */
+
+#define WIFI_SSID "OPPO A53s"
+#define WIFI_PASSWORD "michele06"
 
 #define API_KEY "f2YtnVuyxOV7omZ0zXoaGwcDHgt5Miy12OhW8v8p"
 #define DATABASE_URL "https://hangmangame-it-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -25,7 +28,7 @@ String letturaDato;
 int idDato;
 
 void setup(){
-  Serial.begin(115200);
+  Serial.begin(9600);
   delay(2000);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-Fi");
@@ -51,23 +54,20 @@ void setup(){
 }
 
 void loop(){  
-  x=random(0,9);
-  y=random(10,19);
+  x=random(1, 11);
+  y=random(10, 19);
   
   if (Firebase.ready()) {
     //Firebase.setInt(fbdo, main, 5);
     Firebase.setInt(fbdo, "/test/a", x);
     Firebase.setInt(fbdo, "/test/b", y);
     delay(200);
-
-    Serial.println("Stringa: " + Firebase.getString(fbdo, "/Facile/8") ? fbdo.to<const char *>() : fbdo.errorReason().c_str());
+    String path = "/Facile/" + String(x);
+    Serial.println("Stringa: " + Firebase.getString(fbdo, path ) ? fbdo.to<const char *>() : fbdo.errorReason().c_str());
     letturaDato = fbdo.to<String>();
 
-    Serial.printf("A: %s\n", Firebase.getInt(fbdo, "/test/a") ? String(fbdo.to<int>()).c_str() : fbdo.errorReason().c_str());
-    a=fbdo.to<int>();
-
-    Serial.printf("B: %s\n", Firebase.getInt(fbdo, "/test/b") ? String(fbdo.to<int>()).c_str() : fbdo.errorReason().c_str());
-    b=fbdo.to<int>();
     delay(2500);
   }
+
+  // Serial.println("Lettura dato: " + letturaDato);
 }
