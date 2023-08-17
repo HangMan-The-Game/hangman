b9b0c6ed552742bb89d0fd37c9a4ce2817fb60a0
runzac <91915526+alerunza@users.noreply.github.com>
2023-08-17 18:02:37 +0200

    Update Serial to Serial1 (TX/RX 1)

--- sketch_aug17a/sketch_aug17a.ino
+++ sketch_aug17a/sketch_aug17a.ino
@@ -1,11 +1,12 @@
 void setup() {
   Serial.begin(9600);
+  Serial1.begin(9600);
   Serial.println("Welcome");
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    String receivedString = Serial.readStringUntil('\n');
+  if (Serial1.available() > 0) {
+    String receivedString = Serial1.readStringUntil('\n');
     Serial.println(receivedString);
   }
 }
